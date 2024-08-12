c7f1a6f899b1118ec7c9510757a95da6d4dccb69
Kelwin.Alves <Kelwin.Alves@animaeducacao.com.br>
2024-08-12 19:39:45 -0300

    feature: Implementação de telefones POO

--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Nokia nokia = new Nokia("123","Nk026","0123456789",1234);
+nokia.Ligar();
+nokia.InstalarAplicativo("GOOGLE");
+nokia.ReceberLigacao();
+
+Console.WriteLine();
+Console.WriteLine();
+
+Iphone iphone = new Iphone("123","12","0123456789",1234);
+iphone.Ligar();
+iphone.InstalarAplicativo("Apple Music");
+nokia.ReceberLigacao();
